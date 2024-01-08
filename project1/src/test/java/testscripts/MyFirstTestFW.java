2f0008cc832e66c910aba25b4704b35a5e47688f
Sudheer Guntur <109535753+SudG18@users.noreply.github.com>
2024-01-08 20:28:45 +0530

    Add files via upload
    added whole project

---
+++ project1/src/test/java/testscripts/MyFirstTestFW.java
@@ -0,0 +1,40 @@
+package testscripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+import utilities.ReadXLSData;
+
+public class MyFirstTestFW extends BaseTest {
+		
+		@Test(dataProviderClass=ReadXLSData.class, dataProvider="data")
+		public static void simpleFormValidation(String msg,String a, String b) throws InterruptedException {
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath(loc.getProperty("input_forms_link"))).click();
+		driver.findElement(By.linkText(loc.getProperty("simple_form_demo"))).click();
+		driver.findElement(By.id(loc.getProperty("enter_user_message"))).sendKeys(msg);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath(loc.getProperty("show_message"))).click();
+		String text = driver.findElement(By.cssSelector(loc.getProperty("your_message"))).getText();
+		Assert.assertEquals(text, msg);
+		driver.findElement(By.id(loc.getProperty("value1"))).sendKeys(a);
+		driver.findElement(By.id(loc.getProperty("value2"))).sendKeys(b);
+		driver.findElement(By.xpath(loc.getProperty("get_total"))).click();
+		String sum= driver.findElement(By.id(loc.getProperty("total_value"))).getText();
+		System.out.println(sum);		
+
+		}
+		
+	/*
+	 * @DataProvider(name="inputdata") public Object[]inputData(){ return new
+	 * Object[][] { {"Creating FW!"}, {"Validating FW!"}, {"Finalized FW!"} }; }
+	 */
+	/*
+	 * @DataProvider(name="testdata") public Object[][] testData(){ return new
+	 * Object[][] { {"45","54"}, {"23","32"}, {"4","16"} }; }
+	 */
+}
